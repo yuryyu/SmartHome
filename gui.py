e9a133fb7057f58f4e8912ac091e659f0a0a30dd
Yury Yurchenko <yuzbaster@gmail.com>
2021-06-19 16:35:46 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,6 +2,7 @@ import os
 from sqlite3.dbapi2 import Date
 import sys
 import random
+# pip install pyqt5-tools
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -13,17 +14,16 @@ import time
 from icecream import ic
 from datetime import datetime 
 import data_acq as da
-import copy
+# pip install pyqtgraph
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
 
 global WatMet
 WatMet=True
-
 def time_format():
-    return f'{datetime.now()}  Emulator|> '
-
+    return f'{datetime.now()}  GUI|> '
 ic.configureOutput(prefix=time_format)
 ic.configureOutput(includeContext=False) # use True for including script file context file 
-
 # Creating Client name - should be unique 
 global clientname
 r=random.randrange(1,10000) # for creating unique client ID
@@ -33,7 +33,6 @@ clientname="IOT_clientId-nXLMZeDcjH"+str(r)
 class MC(Mqtt_client):
     def __init__(self):
         super().__init__()
-
     def on_message(self, client, userdata, msg):
             global WatMet
             topic=msg.topic            
@@ -50,64 +49,32 @@ class MC(Mqtt_client):
                 else:
                     mainwin.graphsDock.update_water_meter(m_decode.split(' Water: ')[1])
                     WatMet = True
-
-
-                #mainwin.airconditionDock.update_temp2_win(m_decode.split('Temperature: ')[1].split(' Humidity: ')[0])
    
 class ConnectionDock(QDockWidget):
     """Main """
     def __init__(self,mc):
-        QDockWidget.__init__(self)
-        
+        QDockWidget.__init__(self)        
         self.mc = mc
         self.topic = comm_topic+'#'        
-        self.mc.set_on_connected_to_form(self.on_connected)
-        
+        self.mc.set_on_connected_to_form(self.on_connected)        
         self.eHostInput=QLineEdit()
         self.eHostInput.setInputMask('999.999.999.999')
-        self.eHostInput.setText(broker_ip)
-        
+        self.eHostInput.setText(broker_ip)        
         self.ePort=QLineEdit()
         self.ePort.setValidator(QIntValidator())
         self.ePort.setMaxLength(4)
-        self.ePort.setText(broker_port)
-        
+        self.ePort.setText(broker_port)        
         self.eClientID=QLineEdit()
         global clientname
-        self.eClientID.setText(clientname)
-        
-        #self.eUserName=QLineEdit()
-        #self.eUserName.setText("MATZI")
-        
-        #self.ePassword=QLineEdit()
-        #self.ePassword.setEchoMode(QLineEdit.Password)
-        #self.ePassword.setText("MATZI")
-        
-        #self.eKeepAlive=QLineEdit()
-        #self.eKeepAlive.setValidator(QIntValidator())
-        #self.eKeepAlive.setText("60")
-        
-        #self.eSSL=QCheckBox()
-        
-        #self.eCleanSession=QCheckBox()
-        #self.eCleanSession.setChecked(True)
-        
+        self.eClientID.setText(clientname)        
         self.eConnectButton=QPushButton("Connect", self)
         self.eConnectButton.setToolTip("click me to connect")
         self.eConnectButton.clicked.connect(self.on_button_connect_click)
-        self.eConnectButton.setStyleSheet("background-color: red")
-        
+        self.eConnectButton.setStyleSheet("background-color: red")        
         formLayot=QFormLayout()
         formLayot.addRow("Host",self.eHostInput )
-        formLayot.addRow("Port",self.ePort )
-        #formLayot.addRow("Client ID", self.eClientID)
-        #formLayot.addRow("User Name",self.eUserName )
-        #formLayot.addRow("Password",self.ePassword )
-        #formLayot.addRow("Keep Alive",self.eKeepAlive )
-        #formLayot.addRow("SSL",self.eSSL )
-        #formLayot.addRow("Clean Session",self.eCleanSession )
+        formLayot.addRow("Port",self.ePort )        
         formLayot.addRow("",self.eConnectButton)
-
         widget = QWidget(self)
         widget.setLayout(formLayot)
         self.setTitleBarWidget(widget)
@@ -116,57 +83,35 @@ class ConnectionDock(QDockWidget):
         
     def on_connected(self):
         self.eConnectButton.setStyleSheet("background-color: green")
-        self.eConnectButton.setObjectName('Connected')
+        self.eConnectButton.setText('Connected')
             
     def on_button_connect_click(self):
         self.mc.set_broker(self.eHostInput.text())
         self.mc.set_port(int(self.ePort.text()))
-        self.mc.set_clientName(self.eClientID.text())
-        #self.mc.set_username(self.eUserName.text())
-        #self.mc.set_password(self.ePassword.text())        
+        self.mc.set_clientName(self.eClientID.text())           
         self.mc.connect_to()        
         self.mc.start_listening()
-        time.sleep(2)
+        time.sleep(1)
         if not self.mc.subscribed:
             self.mc.subscribe_to(self.topic)
             
 class StatusDock(QDockWidget):
     """Status """
-
     def __init__(self,mc):
-        QDockWidget.__init__(self)
-        
-        self.mc = mc        
-
+        QDockWidget.__init__(self)        
+        self.mc = mc
         self.eStatusLivinigAirButton=QPushButton("Status living room aircondition")
         self.eStatusRoomAirButton=QPushButton("Status room aircondition")
-        self.eStatusBoilerButton=QPushButton("Status Boiler")
-        
+        self.eStatusBoilerButton=QPushButton("Status Boiler")        
         formLayot=QFormLayout()
         formLayot.addRow("", self.eStatusLivinigAirButton)
         formLayot.addRow("",self.eStatusRoomAirButton)
         formLayot.addRow("",self.eStatusBoilerButton)
-
         widget = QWidget(self)
         widget.setLayout(formLayot)
         self.setTitleBarWidget(widget)
         self.setWidget(widget)     
-        self.setWindowTitle("Status") 
-        
-
-
-        #formLayot=QFormLayout()        
-        #formLayot.addRow("Topic",self.ePublisherTopic)
-        #formLayot.addRow("QOS",self.eQOS)
-        #formLayot.addRow("Retain",self.eRetainCheckbox)
-        #formLayot.addRow("Message",self.eMessageBox)
-        #formLayot.addRow("",self.ePublishButton)
-        
-        
-        #widget = QWidget(self)
-        #widget.setLayout(formLayot)
-        #self.setWidget(widget) 
-        #self.setWindowTitle("Publish")         
+        self.setWindowTitle("Status")       
        
     def on_button_publish_click(self):
         self.mc.publish_to(self.ePublisherTopic.text(), self.eMessageBox.toPlainText())
@@ -174,30 +119,22 @@ class StatusDock(QDockWidget):
         
 class GraphsDock(QDockWidget):
     """Graphs """
-
     def __init__(self,mc):
         QDockWidget.__init__(self)        
-        self.mc = mc
-        
+        self.mc = mc        
         self.eElectricityButton = QPushButton("Show",self)
-        self.eElectricityButton.clicked.connect(self.on_button_Elec_click)
-        
+        self.eElectricityButton.clicked.connect(self.on_button_Elec_click)        
         self.eElectricityText=QLineEdit()
         self.eElectricityText.setText(" ")
-
         self.eWaterButton = QPushButton("Show",self)
-        self.eWaterButton.clicked.connect(self.on_button_water_click)
-        
+        self.eWaterButton.clicked.connect(self.on_button_water_click)        
         self.eWaterText= QLineEdit()
         self.eWaterText.setText(" ")
-
         self.eDate= QLineEdit()
         self.eDate.setText("06-06-2021")
         self.eDateButton=QPushButton("Insert", self)
         self.eDateButton.clicked.connect(self.on_button_date_click)
-
         self.date=self.on_button_date_click
-
         formLayot=QFormLayout()       
         formLayot.addRow("Electricity meter",self.eElectricityButton)
         formLayot.addRow(" ", self.eElectricityText)
@@ -205,7 +142,6 @@ class GraphsDock(QDockWidget):
         formLayot.addRow(" ", self.eWaterText)
         formLayot.addRow("Insert date: ", self.eDate)
         formLayot.addRow("", self.eDateButton)
-
         widget = QWidget(self)
         widget.setLayout(formLayot)
         self.setWidget(widget)
@@ -219,10 +155,7 @@ class GraphsDock(QDockWidget):
 
     def on_button_date_click (self):
         dateStr= self.eDate.text()
-        date=datetime.strptime(dateStr, '%d-%m-%Y')
-        print(date)
-
-        
+        date=datetime.strptime(dateStr, '%d-%m-%Y')        
         return date
 
     def on_button_water_click(self):
@@ -230,30 +163,38 @@ class GraphsDock(QDockWidget):
        self.eWaterButton.setStyleSheet("background-color: yellow")
 
     def on_button_Elec_click(self):
-       da.show_graph('ElecMeter', self.date)
-       self.eElectricityButton.setStyleSheet("background-color: yellow")
-    
+       #da.show_graph('ElecMeter', self.date)
 
-    # create function that update text in received message window
-    #def update_mess_win(self,text):
-    #    self.eRecMess.append(text)
+        rez= da.filter_by_date('data','2021-05-16','2021-05-21', 'ElecMeter')
+        print(rez[1][0])
+        # df = fetch_data(db_name,'data', 'WaterMeter')
+        # ic2(df.head())
+
+        temperature = []  
+        timenow = []
+        import dateutil
+        for row in rez:
+            timenow.append(row[1])
+            temperature.append(float("{:.2f}".format(float(row[2]))))
+        print(timenow)
+        print(temperature)
+
+        mainwin.plotsDock.plot(timenow, temperature) 
+
+        self.eElectricityButton.setStyleSheet("background-color: yellow")   
 
 class TempDock(QDockWidget):
     """Temp """
-
     def __init__(self,mc):
         QDockWidget.__init__(self)        
-        self.mc = mc
-    
+        self.mc = mc    
         self.eBoilerButton = QPushButton("Boiler",self)
         self.eFreezerButton = QPushButton("Freezer",self)
-        self.eRefrigeratorButton = QPushButton("Refrigerator",self)
-        
+        self.eRefrigeratorButton = QPushButton("Refrigerator",self)        
         formLayot=QFormLayout()       
         formLayot.addRow("Boiler",self.eBoilerButton)
         formLayot.addRow("Room Freezer",self.eFreezerButton)
         formLayot.addRow("Refrigerator",self.eRefrigeratorButton)
-
         widget = QWidget(self)
         widget.setLayout(formLayot)
         self.setWidget(widget)
@@ -261,25 +202,20 @@ class TempDock(QDockWidget):
 
 class AirconditionDock(QDockWidget):
     """Aircondition """
-
     def __init__(self,mc):
         QDockWidget.__init__(self)        
-        self.mc = mc
-    
+        self.mc = mc    
         self.eLivingButton = QPushButton("Living Room",self)
         self.eLivRoomTemp=QLineEdit()
         self.eLivRoomTemp.setText(" ")
-
         self.eRoomgButton = QPushButton("Room",self)        
         self.eRoomTemp=QLineEdit()
         self.eRoomTemp.setText(" ")
-
         formLayot=QFormLayout()       
         formLayot.addRow("Living Room Temperature", self.eLivingButton)
         formLayot.addRow(" ", self.eLivRoomTemp)
         formLayot.addRow("Room Temperature",self.eRoomgButton)
         formLayot.addRow(" ", self.eRoomTemp)
-
         widget = QWidget(self)
         widget.setLayout(formLayot)
         self.setWidget(widget)
@@ -287,40 +223,68 @@ class AirconditionDock(QDockWidget):
 
     def update_temp_Living_Room(self,text):
         self.eLivRoomTemp.setText(text)
+
     def update_temp_Room(self, text):
         self.eRoomTemp.setText(text)    
 
-class MainWindow(QMainWindow):
-    
+class PlotDock(QDockWidget):
+    """Plots """
+    def __init__(self):
+        QDockWidget.__init__(self)        
+        self.setWindowTitle("Graphs")
+        self.graphWidget = pg.PlotWidget()
+        self.setWidget(self.graphWidget)
+        rez= da.filter_by_date('data','2021-05-16','2021-05-18', 'ElecMeter')
+        print(rez[1][0])
+        # df = fetch_data(db_name,'data', 'WaterMeter')
+        # ic2(df.head())
+
+        datal = []  
+        timel = []
+        # import dateutil
+        # import numpy as np
+        
+        for row in rez:
+            timel.append(row[1])
+            datal.append(float("{:.2f}".format(float(row[2]))))
+        # tn= np.array(timel).astype(np.float)
+        #nn=np.array(datal).astype(np.float)
+
+        self.graphWidget.setBackground('b')            
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line=self.graphWidget.plot( datal,  pen=pen)
+
+    def plot(self, timel, datal):
+        self.data_line.setData( datal)  # Update the data.
+
+
+class MainWindow(QMainWindow):    
     def __init__(self, parent=None):
-        QMainWindow.__init__(self, parent)
-                
+        QMainWindow.__init__(self, parent)                
         # Init of Mqtt_client class
         # self.mc=Mqtt_client()
-        self.mc=MC()
-        
+        self.mc=MC()        
         # general GUI settings
         self.setUnifiedTitleAndToolBarOnMac(True)
-
         # set up main window
         self.setGeometry(30, 100, 800, 600)
-        self.setWindowTitle('System GUI')        
-
+        self.setWindowTitle('System GUI')
         # Init QDockWidget objects        
         self.connectionDock = ConnectionDock(self.mc)        
         self.statusDock = StatusDock(self.mc)
         self.tempDock = TempDock(self.mc)
         self.graphsDock = GraphsDock(self.mc)
         self.airconditionDock= AirconditionDock(self.mc)
-
+        self.plotsDock = PlotDock()
         self.addDockWidget(Qt.TopDockWidgetArea, self.connectionDock)
         self.addDockWidget(Qt.TopDockWidgetArea, self.tempDock)
         self.addDockWidget(Qt.TopDockWidgetArea, self.airconditionDock)
         self.addDockWidget(Qt.BottomDockWidgetArea, self.statusDock)
         self.addDockWidget(Qt.BottomDockWidgetArea, self.graphsDock)
-       
+        self.addDockWidget(Qt.BottomDockWidgetArea, self.plotsDock)       
 
-app = QApplication(sys.argv)
-mainwin = MainWindow()
-mainwin.show()
-app.exec_()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    mainwin = MainWindow()
+    mainwin.show()
+    app.exec_()

89130b2aab7497594160d7519587efe16d701028
Yury Yurchenko (Consultant) <c_yuryy@qti.qualcomm.com>
2021-06-26 16:20:57 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -103,20 +103,24 @@ class StatusDock(QDockWidget):
     def __init__(self,mc):
         QDockWidget.__init__(self)        
         self.mc = mc
-        self.eStatusLivinigAirButton=QPushButton("Status living room aircondition")
-        self.eStatusRoomAirButton=QPushButton("Status room aircondition")
-        self.eStatusBoilerButton=QPushButton("Status Boiler") 
+        self.boilerTemp = QLabel()
+        self.boilerTemp.setText("80")
+        self.boilerTemp.setStyleSheet("color: red")
+        self.wifi = QLabel()
+        self.wifi.setText("Normal")
+        self.wifi.setStyleSheet("color: green")
+        self.door = QLabel()
+        self.door.setText("Closed")
+        self.door.setStyleSheet("color: green")
         self.eRecMess=QTextEdit()
         self.eSubscribeButton = QPushButton("Subscribe",self)
         self.eSubscribeButton.clicked.connect(self.on_button_subscribe_click)       
         formLayot=QFormLayout()
-
-        formLayot.addRow("", self.eStatusLivinigAirButton)
-        formLayot.addRow("",self.eStatusRoomAirButton)
-        formLayot.addRow("",self.eStatusBoilerButton)        
-        formLayot.addRow("Alarm:",self.eRecMess)
-        formLayot.addRow("",self.eSubscribeButton)        
-                
+        formLayot.addRow("Boiler temperature:", self.boilerTemp)
+        formLayot.addRow("WI-Fi status:",self.wifi)
+        formLayot.addRow("Main Door:",self.door)        
+        formLayot.addRow("Alarm Messages:",self.eRecMess)
+        formLayot.addRow("",self.eSubscribeButton)                
         widget = QWidget(self)
         widget.setLayout(formLayot)
         self.setTitleBarWidget(widget)
@@ -204,59 +208,153 @@ class TempDock(QDockWidget):
     def __init__(self,mc):
         QDockWidget.__init__(self)        
         self.mc = mc    
-        self.eBoilerButton = QPushButton("Boiler",self)
-        self.eFreezerButton = QPushButton("Freezer",self)
-        self.eRefrigeratorButton = QPushButton("Refrigerator",self)        
+        
+        self.tBoiler = QComboBox()        
+        self.tBoiler.addItems(["Auto", "ON", "OFF"])
+        self.tBoiler.currentIndexChanged.connect(self.tb_selectionchange)        
+        self.tFreezer = QComboBox()        
+        self.tFreezer.addItems(["-5", "-10", "-15"])
+        self.tRefrigerator = QComboBox()
+        self.tRefrigerator.addItems(["4", "3", "2", "1", "0", "-1", "-2", "-3", "-4"])
+        self.tsetButton = QPushButton("SET(UPDATE)",self)
+        self.tsetButton.clicked.connect(self.on_tsetButton_click)
+
         formLayot=QFormLayout()       
-        formLayot.addRow("Boiler",self.eBoilerButton)
-        formLayot.addRow("Room Freezer",self.eFreezerButton)
-        formLayot.addRow("Refrigerator",self.eRefrigeratorButton)
+        formLayot.addRow("Home Boiler",self.tBoiler)
+        formLayot.addRow("Kitchen Freezer",self.tFreezer)
+        formLayot.addRow("Refrigerator",self.tRefrigerator)
+        formLayot.addRow("",self.tsetButton)
+
         widget = QWidget(self)
         widget.setLayout(formLayot)
         self.setWidget(widget)
-        self.setWindowTitle("Temperature")
+        self.setWindowTitle("Set Temperature")
+
+    def on_tsetButton_click(self):
+        self.tsetButton.setStyleSheet("background-color: green") 
+
+    def tb_selectionchange(self,i):
+        print ("Current index",i,"selection changed ",self.tBoiler.currentText())
+        if "ON" in self.tBoiler.currentText():
+            self.tBoiler.setStyleSheet("color: green")
+        elif "OFF" in self.tBoiler.currentText():
+            self.tBoiler.setStyleSheet("color: red")
+        else:
+            self.tBoiler.setStyleSheet("color: none") 
 
 class AirconditionDock(QDockWidget):
     """Aircondition """
     def __init__(self,mc):
         QDockWidget.__init__(self)        
-        self.mc = mc    
-        self.eLivingButton = QPushButton("Living Room",self)
-        self.eLivRoomTemp=QLineEdit()
-        self.eLivRoomTemp.setText(" ")
-        self.eRoomgButton = QPushButton("Room",self)        
-        self.eRoomTemp=QLineEdit()
-        self.eRoomTemp.setText(" ")
-        # formLayot=QFormLayout()       
-        # formLayot.addRow("Living Room Temperature", self.eLivingButton)
-        # formLayot.addRow(" ", self.eLivRoomTemp)
-        # formLayot.addRow("Room Temperature",self.eRoomgButton)
-        # formLayot.addRow(" ", self.eRoomTemp)
-
+        self.mc = mc
+        # Line #1
+        self.l1 = QLabel()
+        self.l1.setText("PLACE:")
+        self.l1.setFont(QFont('Arial', 10))
+        self.l1.setStyleSheet("color: rgb(0, 0, 255);")
+        # self.l1.setAlignment(Qt.AlignCenter)
+        self.cb = QComboBox()        
+        self.cb.addItems(["Living Room", "Room 1", "Room 2"])
+        self.cb.currentIndexChanged.connect(self.selectionchange)
+		# Line #2
+        self.l21 = QLabel()
+        self.l21.setText("Temperature: Current")
+        self.cRoomTemp=QLineEdit()
+        self.cRoomTemp.setText(" ")
+        self.l22 = QLabel()
+        self.l22.setText("Target")        
+        self.tRoomTemp = QComboBox()        
+        self.tRoomTemp.addItems(["min", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "max"])
+        self.tRoomTemp.currentIndexChanged.connect(self.tr_selectionchange)    
+        # Line #3
+        self.l31 = QLabel()
+        self.l31.setText("Mode")
+        self.md = QComboBox()        
+        self.md.addItems(["Cool", "Heat", "Dry","Fan"])
+        self.md.currentIndexChanged.connect(self.md_selectionchange)
+        self.l32 = QLabel()
+        self.l32.setText("Fan")
+        self.fn = QComboBox()        
+        self.fn.addItems(["High", "Middle", "Low"])
+        self.fn.currentIndexChanged.connect(self.fn_selectionchange)
+        # Line #4
+        self.l41 = QLabel()
+        self.l41.setText("ON\OFF:")
+        self.od = QComboBox()        
+        self.od.addItems(["AUTO", "OFF", "ON"])        
+        self.od.currentIndexChanged.connect(self.od_selectionchange)
+        self.l42 = QLabel()
+        self.l42.setText("Status:")
+        self.st = QComboBox()        
+        self.st.addItems(["Unknown", "Failure", "Normal"])
+        self.st.currentIndexChanged.connect(self.st_selectionchange)
+        # Line #5
+        self.setButton = QPushButton("SET(UPDATE)",self)
+        self.setButton.clicked.connect(self.on_setButton_click)
         layout = QGridLayout()
         # Add widgets to the layout
-        layout.addWidget(self.eLivingButton, 0, 0)
-        layout.addWidget(self.eLivRoomTemp, 0, 1)
-        layout.addWidget(self.eRoomgButton, 0, 2)
-        layout.addWidget(self.eRoomTemp, 1, 0)
-        layout.addWidget(QPushButton("Button at (1, 1)"), 1, 1)
-        layout.addWidget(QPushButton("Button at (1, 2)"), 1, 2)
-        layout.addWidget(QPushButton("Button at (2, 0)"), 2, 0)
-        layout.addWidget(QPushButton("Button at (2, 1)"), 2, 1)
-        layout.addWidget(QPushButton("Button at (2, 2)"), 2, 2)
+        # Line #1
+        layout.addWidget(self.l1, 0,1)
+        layout.addWidget(self.cb, 0,2)
+        # Line #2 
+        layout.addWidget(self.l21, 1,0)
+        layout.addWidget(self.cRoomTemp, 1,1)
+        layout.addWidget(self.l22, 1,2)
+        layout.addWidget(self.tRoomTemp, 1,3)
+        # Line #3 
+        layout.addWidget(self.l31, 2,0)
+        layout.addWidget(self.md, 2,1)
+        layout.addWidget(self.l32, 2,2)
+        layout.addWidget(self.fn, 2,3)
+        # Line #4 
+        layout.addWidget(self.l41, 3,0)
+        layout.addWidget(self.od, 3,1)
+        layout.addWidget(self.l42, 3,2)
+        layout.addWidget(self.st, 3,3)
+        # Line #5 
+        layout.addWidget(self.setButton, 4,1,4,2)       
         # Set the layout on the application's window
         self.setLayout(layout)
-
         widget = QWidget(self)
         widget.setLayout(layout)
         self.setWidget(widget)
         self.setWindowTitle("Aircondition")
 
-    def update_temp_Living_Room(self,text):
-        self.eLivRoomTemp.setText(text)
-
     def update_temp_Room(self, text):
-        self.eRoomTemp.setText(text)    
+        self.cRoomTemp.setText(text)  
+
+    def selectionchange(self,i):
+        print ("Current index",i,"selection changed ",self.cb.currentText())
+
+    def md_selectionchange(self,i):
+        print ("Current index",i,"selection changed ",self.md.currentText())
+
+    def fn_selectionchange(self,i):
+        print ("Current index",i,"selection changed ",self.fn.currentText())
+
+    def od_selectionchange(self,i):
+        print ("Current index",i,"selection changed ",self.od.currentText())
+        if "ON" in self.od.currentText():
+            self.od.setStyleSheet("color: green")
+        elif "OFF" in self.od.currentText():
+            self.od.setStyleSheet("color: red")
+        else:
+            self.od.setStyleSheet("color: none") 
+
+        #setStyleSheet("color: blue;"
+        #                "background-color: yellow;"
+        #                "selection-color: yellow;"
+        #                "selection-background-color: blue;");    
+
+    def st_selectionchange(self,i):
+        print ("Current index",i,"selection changed ",self.st.currentText())  
+
+    def tr_selectionchange(self,i):
+        print ("Current index",i,"selection changed ",self.tRoomTemp.currentText())  
+
+    def on_setButton_click(self):
+        self.setButton.setStyleSheet("background-color: yellow")             
+      
 
 class PlotDock(QDockWidget):
     """Plots """

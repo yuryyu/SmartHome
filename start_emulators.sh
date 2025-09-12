d8fd1c0fda2f9f6622e32fa7b01c01b000d893d6
Yury <yuzbaster@gmail.com>
2025-09-12 16:35:12 +0300

    ssch

--- start_emulators.sh
+++ start_emulators.sh
@@ -5,8 +5,6 @@ sleep 1
 system_profiler SPHardwareDataType | grep -i Identifier
 sleep 1
 
-
-
 osascript -e 'tell app "Terminal"to do script "/Users/yuryyim/Documents/GitHub/SmartHome/venv/bin/python /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py DHT-1 Celsius Room_1 7"'
 sleep 1
 osascript -e 'tell app "Terminal" to do script "/Users/yuryyim/Documents/GitHub/SmartHome/venv/bin/python /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py DHT-2 Celsius Room_2 5"'

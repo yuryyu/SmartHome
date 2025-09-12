db190d56c27e7a3e5bcad3f8dd92523bf7349ee6
Yury <yuzbaster@gmail.com>
2025-09-12 16:32:23 +0300

    Update start_emulators.sh

--- start_emulators.sh
+++ start_emulators.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-#diskutil info disk0 | grep -i smart
-#sleep 1
-#system_profiler SPHardwareDataType | grep -i Identifier
-#sleep 1
-cd /Users/yuryyim/Documents/GitHub/SmartHome
+diskutil info disk0 | grep -i smart
+sleep 1
+system_profiler SPHardwareDataType | grep -i Identifier
+sleep 1
 
-osascript -e 'tell app "Terminal"
-    do script "/opt/homebrew/bin/python3 /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py DHT-1 Celsius Room_1 7"
-end tell'
+
+
+osascript -e 'tell app "Terminal"to do script "/Users/yuryyim/Documents/GitHub/SmartHome/venv/bin/python /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py DHT-1 Celsius Room_1 7"'
 sleep 1
-osascript -e 'tell app "Terminal" to do script "/opt/homebrew/bin/python3 /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py DHT-12 Celsius Room_2 5"'
+osascript -e 'tell app "Terminal" to do script "/Users/yuryyim/Documents/GitHub/SmartHome/venv/bin/python /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py DHT-2 Celsius Room_2 5"'
 sleep 1
-osascript -e 'tell app "Terminal" to do script "/opt/homebrew/bin/python3 /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py ElecWaterMeter kWh Home 13"'
+osascript -e 'tell app "Terminal" to do script "/Users/yuryyim/Documents/GitHub/SmartHome/venv/bin/python /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py ElecWaterMeter kWh Home 13"'
 sleep 1
-osascript -e 'tell app "Terminal" to do script "/opt/homebrew/bin/python3 /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py Airconditioner Celsius air-1 5"'
+osascript -e 'tell app "Terminal" to do script "/Users/yuryyim/Documents/GitHub/SmartHome/venv/bin/python /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py Airconditioner Celsius air-1 5"'
 sleep 1
-osascript -e 'tell app "Terminal" to do script "/opt/homebrew/bin/python3 /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py Freezer Celsius freezer 6"'
+osascript -e 'tell app "Terminal" to do script "/Users/yuryyim/Documents/GitHub/SmartHome/venv/bin/python /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py Freezer Celsius freezer 6"'
 sleep 1
-osascript -e 'tell app "Terminal" to do script "/opt/homebrew/bin/python3 /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py Boiler Celsius boiler 8"'
+osascript -e 'tell app "Terminal" to do script "/Users/yuryyim/Documents/GitHub/SmartHome/venv/bin/python /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py Boiler Celsius boiler 8"'
 sleep 1
-osascript -e 'tell app "Terminal" to do script "/opt/homebrew/bin/python3 /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py Refrigerator Celsius refrigerator 9"'
+osascript -e 'tell app "Terminal" to do script "/Users/yuryyim/Documents/GitHub/SmartHome/venv/bin/python /Users/yuryyim/Documents/GitHub/SmartHome/emulator.py Refrigerator Celsius refrigerator 9"'
 sleep 1
-osascript -e 'tell app "Terminal" to do script "/opt/homebrew/bin/python3 /Users/yuryyim/Documents/GitHub/SmartHome/manager.py"'
+osascript -e 'tell app "Terminal" to do script "/Users/yuryyim/Documents/GitHub/SmartHome/venv/bin/python /Users/yuryyim/Documents/GitHub/SmartHome/manager.py"'
 sleep 1
-osascript -e 'tell app "Terminal" to do script "/opt/homebrew/bin/python3 /Users/yuryyim/Documents/GitHub/SmartHome/gui.py"'
+osascript -e 'tell app "Terminal" to do script "/Users/yuryyim/Documents/GitHub/SmartHome/venv/bin/python /Users/yuryyim/Documents/GitHub/SmartHome/gui.py"'
 
 
 
